fe530a2933ad8bf77427ad73034dcea5cc3948b7
smvfal <smvfal@gmail.com>
2021-02-24 16:24:19 +0100

    publish json format metrics to nats

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/smvfal/faas-monitor/metrics"
+	"github.com/smvfal/faas-monitor/nats"
 	"log"
 	"time"
 )
 
+type function struct {
+	Name     string `json:"name"`
+	Replicas int    `json:"replicas"`
+}
+
 func main() {
+
 	var p metrics.Provider
 	p = &metrics.FaasProvider{}
 
@@ -18,14 +26,24 @@ func main() {
 			log.Fatal(err.Error())
 		}
 
-		for _, function := range functions {
+		for _, fname := range functions {
 
-			replicas, err := p.FunctionReplicas(function)
+			f := function{Name: fname}
+			f.Replicas, err = p.FunctionReplicas(f.Name)
 			if err != nil {
 				log.Println(err.Error())
 			}
 
-			fmt.Printf("%s: %d replicas\n", function, replicas)
+			fmt.Printf("%s: %d replicas\n", f.Name, f.Replicas)
+
+			// marshal to json
+			fjson, err := json.Marshal(f)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+			fmt.Println(string(fjson))
+
+			nats.Publish(fjson)
 		}
 
 		time.Sleep(10 * time.Second)
